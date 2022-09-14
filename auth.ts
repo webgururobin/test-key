f271b352cca7a6efab00fb2a1a5f0122789bf06b
Robin Solanki <web.guru@me.com>
2022-09-15 04:46:24 +0530

    Change session key

--- auth.ts
+++ auth.ts
@@ -17,28 +17,26 @@ let sessionSecret = process.env.SESSION_SECRET;
 // Here is a best practice! It's fine to not have provided a session secret in dev,
 // however it should always be there in production.
 if (!sessionSecret) {
-  if (process.env.NODE_ENV === 'production') {
-    throw new Error(
-      'The SESSION_SECRET environment variable must be set in production'
-    );
-  } else {
-    sessionSecret = '-- DEV COOKIE SECRET; CHANGE ME --';
-  }
+	if (process.env.NODE_ENV === 'production') {
+		throw new Error('The SESSION_SECRET environment variable must be set in production');
+	} else {
+		sessionSecret = 'test';
+	}
 }
 
 // Here we define how auth relates to our schemas.
 // What we are saying here is that we want to use the list `User`, and to log in
 // we will need their email and password.
 const { withAuth } = createAuth({
-  listKey: 'User',
-  identityField: 'email',
-  sessionData: 'name',
-  secretField: 'password',
-  initFirstItem: {
-    // If there are no items in the database, keystone will ask you to create
-    // a new user, filling in these fields.
-    fields: ['name', 'email', 'password'],
-  },
+	listKey: 'User',
+	identityField: 'email',
+	sessionData: 'name',
+	secretField: 'password',
+	initFirstItem: {
+		// If there are no items in the database, keystone will ask you to create
+		// a new user, filling in these fields.
+		fields: ['name', 'email', 'password'],
+	},
 });
 
 // This defines how long people will remain logged in for.
@@ -47,8 +45,8 @@ let sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
 
 // This defines how sessions should work. For more details, check out: https://keystonejs.com/docs/apis/session#session-api
 const session = statelessSessions({
-  maxAge: sessionMaxAge,
-  secret: sessionSecret!,
+	maxAge: sessionMaxAge,
+	secret: sessionSecret!,
 });
 
 export { withAuth, session };
